3a68ef1bcfeaad9bc19b37c1f4619ac3652bd506
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-06-29 00:49:18 +0500

    cors problem fixed

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"ai_marketplace_go/config"
 	_ "ai_marketplace_go/docs"
 	"ai_marketplace_go/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 // @title Go Gin Auth API
@@ -15,6 +17,16 @@ import (
 // @BasePath /
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	db := config.InitDB()
 
 	routes.SetupRoutes(r, db)
